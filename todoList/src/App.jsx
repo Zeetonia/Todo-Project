aa4d43b22ecc6079267529f9c033301dd29c1199
Zeetonia <zaher.lp@gmail.com>
2024-04-09 00:25:18 +0300

    last added is add btn

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -1,3 +1,11 @@
+import "./styles.css"
+
 export default function App () {
-  return "Hi"
+  return <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New item</label>
+      <input type="text" name="item" id="item" />
+    </div>
+    <button className="btn">Add</button>
+  </form>
 }
